6e4c2cc7b6ec748b643fb0a1e8afe00940b155b4
Himesh Patel <itshimesh@users.noreply.github.com>
2016-11-02 08:59:44 -0400

    finished gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
 module.exports = function(grunt) {
 
   // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
   // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+  // require('time-grunt')(grunt);
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      files: ['Gruntfile.js', 'src/js/*.js'],
+      files: ['src/js/*.js'],
       options: {
         "curly": true,
         "eqeqeq": true,
@@ -19,19 +19,35 @@ module.exports = function(grunt) {
         }
       }
     },
+    sass: {
+        pretty: {
+            src: ['src/sass/style.scss'],
+            dest: 'css/style.css'
+        },
+        minify: {
+            options: {
+                style: 'compressed'
+            },
+            src: ['src/sass/style.scss'],
+            dest: 'css/style.css'
+        }
+    },
     uglify: {
+      options: {
+        compress: true
+      },
       my_target: {
           files: {
-              '_/js/script.js': ['_/components/js/*.js'] //compresses and combine multiple js files
-          } //files
-      } //my_target
-    }, //uglify
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+              'js/script.js': ['src/js/*.js']
+          }
+      }
     }
+    // watch: {
+    //   files: ['<%= jshint.files %>'],
+    //   tasks: ['jshint']
+    // }
   });
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'sass', 'uglify']);
 
 };
